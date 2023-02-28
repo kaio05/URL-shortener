ade9017d0e4cadb14d19369afe019bbd2df42109
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-02-28 09:53:36 -0300

    javascript shrotening api test
    learning about api with the shrtco.de

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-const linkButton = document.querySelector('.shorten-btn');
-const input = document.querySelector('.input');
+// const linkButton = document.querySelector('.shorten-btn');
+// const input = document.querySelector('.input');
+// const errorMessage = document.querySelector('.error-message');
 
-linkButton.addEventListener('click', () => {
-    link = input.value;
-    validateURL();
-})
+// linkButton.addEventListener('click', () => {
+//     //verifyURL();
+    
+// });
 
-const validateURL = () => {
-    alert('hello world!')
+
+let url = "https://www.youtube.com/cursoemvideo";    
+
+let request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
+
+let response = await request.json();
+
+let shortened = response.result.full_short_link;
+
+console.log(shortened);
+
+/*
+function verifyURL() {
+    let link = String(input.value);
+    if (link.length == 0) {
+        input.classList.add('error');
+        errorMessage.classList.add('show');
+    } else {
+        input.classList.remove('error');
+        errorMessage.classList.remove('show');
+    }
 }
+
+function generateLink() {
+    alert('hello');
+} 
+*/
