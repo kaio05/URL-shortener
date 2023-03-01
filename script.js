20741948fe39b3a89656530b14b001176d491ed7
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-03-01 10:23:15 -0300

    successfully got api response
    To do:
    ## Now
    js - get the shortened link by returning it's value
    
    html + css - make the mobile version of the static site
    
    ## Then
    js - dynamically generate the link containers and display 'em on the page
    
    js - use api to save history log

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-// const linkButton = document.querySelector('.shorten-btn');
-// const input = document.querySelector('.input');
-// const errorMessage = document.querySelector('.error-message');
+const linkButton = document.querySelector('.shorten-btn');
+const input = document.querySelector('.input');
+const errorMessage = document.querySelector('.error-message');
 
-// linkButton.addEventListener('click', () => {
-//     //verifyURL();
-    
-// });
+linkButton.addEventListener('click', async() => {
+    shortenURL(input.value);
+});
+
+async function shortenURL(url) {  
+
+    let link = url
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(link)}`);
+    xhr.send();
+    xhr.responseType = 'json';
+    xhr.onload = () => {
+        if (xhr.readyState == 4 && xhr.status == 201) {
+            const shortenLink = xhr.response.full_shorten_link;
+            input.classList.remove('error');
+            errorMessage.classList.remove('show');
+            return shortenLink;
+        } else {
+            console.log(`Error: ${xhr.status}`);
+            input.classList.add('error');
+            errorMessage.classList.add('show');
+            return false;
+        }
+    }
+};
+ 
+
+// const getURL = fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`)
+//     .then(response => response.json())
+//     .then(result => console.log(result.full_short_link))
+// }
+    // const getShort = GET/POST (`https://api.shrtco.de/v2/shorten?url=${link}`)
+    //     .then(response => response.json())
+    //     .then(result => console.log(result.full_short_link))
+
+    // let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(link)}`);
+    // let shortened = await response.json();
+    // return shortened.full_short_link;
+
+    // let request =  await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
+    // let response = (await request.json());
+    // console.log(response.full_short_link);
+
+    // let request = (`https://api.shrtco.de/v2/shorten?url=${link}`)
+    // let response = await request.full_short_link
 
 
-let url = "https://www.youtube.com/cursoemvideo";    
 
-let request = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`);
-
-let response = await request.json();
-
-let shortened = response.result.full_short_link;
-
-console.log(shortened);
-
+// console.log(shortenURL("https://www.cursoemvideo.com/")); 
 /*
 function verifyURL() {
     let link = String(input.value);
