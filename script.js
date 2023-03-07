72a2349fd43640383c49a91c588ea86271c4bc93
kaio05 <98607741+kaio05@users.noreply.github.com>
2023-03-07 15:06:39 -0300

    copy button selection system working!

--- script.js
+++ script.js
@@ -1,69 +1,100 @@
 const linkButton = document.querySelector('.shorten-btn');
 const input = document.querySelector('.input');
 const errorMessage = document.querySelector('.error-message');
+const linkArea = document.querySelector('.link-copying-area')
 
-linkButton.addEventListener('click', async() => {
+linkButton.onclick = () => {
+    shortenIt();
+}
+
+input.addEventListener('keypress', e => {
+    if (e.key == 'Enter') {
+        shortenIt();
+    }
+})
+
+async function shortenIt() {
+    //console.log('calling');
     shortenURL(input.value);
-});
+};
 
 async function shortenURL(url) {  
-
     let link = url
     const xhr = new XMLHttpRequest();
     xhr.open('GET', `https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(link)}`);
     xhr.send();
     xhr.responseType = 'json';
-    xhr.onload = () => {
-        if (xhr.readyState == 4 && xhr.status == 201) {
-            const shortenLink = xhr.response.full_shorten_link;
-            input.classList.remove('error');
-            errorMessage.classList.remove('show');
-            return shortenLink;
-        } else {
-            console.log(`Error: ${xhr.status}`);
-            input.classList.add('error');
-            errorMessage.classList.add('show');
-            return false;
+    xhr.onload = await function f() {
+            if (xhr.readyState == 4 && xhr.status == 201 || xhr.readyState == 4 && xhr.status == 200) {
+                const data = xhr.response.result;
+                const fullShortLink = data.full_short_link;
+                createContainer(fullShortLink, link);
+                urlSuccess();
+                //console.log(xhr.status);
+            } else {
+                //console.log(`Error: ${xhr.status}`);
+                urlError()
+            }
         }
-    }
 };
- 
-
-// const getURL = fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(url)}`)
-//     .then(response => response.json())
-//     .then(result => console.log(result.full_short_link))
-// }
-    // const getShort = GET/POST (`https://api.shrtco.de/v2/shorten?url=${link}`)
-    //     .then(response => response.json())
-    //     .then(result => console.log(result.full_short_link))
-
-    // let response = await fetch(`https://api.shrtco.de/v2/shorten?url=${encodeURIComponent(link)}`);
-    // let shortened = await response.json();
-    // return shortened.full_short_link;
-
-    // let request =  await fetch(`https://api.shrtco.de/v2/shorten?url=${link}`);
-    // let response = (await request.json());
-    // console.log(response.full_short_link);
-
-    // let request = (`https://api.shrtco.de/v2/shorten?url=${link}`)
-    // let response = await request.full_short_link
 
 
 
-// console.log(shortenURL("https://www.cursoemvideo.com/")); 
-/*
-function verifyURL() {
-    let link = String(input.value);
-    if (link.length == 0) {
-        input.classList.add('error');
-        errorMessage.classList.add('show');
-    } else {
-        input.classList.remove('error');
-        errorMessage.classList.remove('show');
-    }
+function createContainer(short, original) {
+    const container = document.createElement('div');
+    const containers = document.querySelectorAll('.linkcontainer');
+    container.classList.add('link-container');
+    
+    const URLContainer = document.createElement('span');
+    URLContainer.classList.add('original-url');
+    URLContainer.innerHTML = original;
+    
+    const shortContainer = document.createElement('span');
+    shortContainer.classList.add('shorten-link');
+    shortContainer.innerHTML = short;
+    
+    container.appendChild(URLContainer);
+    container.appendChild(shortContainer);
+    linkArea.appendChild(container);
+
+    copyButtonFunction(shortContainer, container);
 }
 
-function generateLink() {
-    alert('hello');
-} 
-*/
+function copyButtonFunction(shortContainer, container) {
+    const copyBtn = document.createElement('button');
+    copyBtn.className = 'copy-btn';
+    copyBtn.innerHTML = 'Copy';
+    container.appendChild(copyBtn);
+    let buttons = document.querySelectorAll('.copy-btn');
+    
+    buttons.forEach(pbuttons => {
+        pbuttons.addEventListener('click', e => {
+            let copyText = shortContainer;
+            let textArea = document.createElement('textarea');
+            textArea.value = copyText.innerHTML;
+            document.body.appendChild(textArea);
+            textArea.select();
+            navigator.clipboard.writeText(textArea.value);
+            textArea.remove();
+
+            let button = e.currentTarget;
+            buttons.forEach(btn => btn != button && btn.classList.remove('checked'));
+            button.classList.add('checked');
+            console.log(button);
+
+            // credits: https://stackoverflow.com/questions/73754798/how-can-i-deselect-one-button-when-i-select-another
+    });
+    });
+    
+    console.log(buttons)
+}
+
+function urlSuccess() {
+    input.classList.remove('error');
+    errorMessage.classList.remove('show');
+}
+
+function urlError() {
+    input.classList.add('error');
+    errorMessage.classList.add('show');
+}
